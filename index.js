77d72590d475ed131bd9f0ce23fc3807e8511589
sonolibero.io <ciao@sonolibero.io>
2022-10-27 14:52:18 +0200

    organize

--- index.js
+++ index.js
@@ -1,40 +1,6 @@
-function Header() {
-    return (
-        <header>
-            <nav className="nav">
-                <img src="./react-logo.png" className="nav-logo" />
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function MainContent() {
-    return (
-        <div>
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </div>
-    )
-}
-
-function Footer() {
-    return (
-        <footer>
-            <small>© 2022 clod. All rights reserved.</small>
-        </footer>
-    )
-}
+import Header from "./Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
 
 function Page() {
     return (
