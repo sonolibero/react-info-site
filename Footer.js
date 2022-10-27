8dcc9285ed2189506ee63ca04be4fe10276d92d1
sonolibero.io <ciao@sonolibero.io>
2022-10-27 15:53:58 +0200

    project setup

--- Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return (
-        <footer>
-            <small>© 2022 clod. All rights reserved.</small>
-        </footer>
-    )
-}
