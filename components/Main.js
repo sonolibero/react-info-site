a8fc1524f21c516742ecaeb1818f125b45df1a9e
sonolibero.io <ciao@sonolibero.io>
2022-10-27 16:55:49 +0200

    done

--- components/Main.js
+++ components/Main.js
@@ -1,14 +1,14 @@
 export default function Main() {
     return (
-        <div>
-            <h1>Fun facts about React</h1>
-            <ul>
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
                 <li>released in 2013</li>
                 <li>created by Jordan Walke</li>
                 <li>over 100K stars on GitHub</li>
                 <li>maintained by Facebook</li>
                 <li>power enterprise and mobile apps</li>
             </ul>
-        </div>
+        </main>
     )
 }
