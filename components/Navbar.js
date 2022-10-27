3d07fb0b9f721b6f9cb580f03e39c6f99787568d
sonolibero.io <ciao@sonolibero.io>
2022-10-27 17:51:59 +0200

    fix icon navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function NavBar() {
     return (
         <nav>
-            <img src="../images/react-icon-small-png" width="40px" className="nav--icon" />
+            <img src="../images/react-icon-small.png" width="40px" className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
